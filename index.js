b9ba924fc70ca6aa86de6ad07b53d37a8619c0b1
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-01 10:42:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var ranksParent = document.querySelectorAll('.ranked-list');
   for(var i = 0; i < ranksParent.length; i++) {
-    var ranks = ranksParent.querySelectorAll('li');
+//    var ranks = ranksParent.querySelectorAll('li');
     for(var idx = 0; idx < ranks.length;idx++) {
       ranks[i].textContent = (parseInt(ranks[i].textContent) + parseInt(n));
     }
