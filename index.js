9902b724ae91e340112ef14b3efdac9caf44ea6a
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-01 11:42:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function deepestChild() {
     // filter on it.
     
     nextNode = queue.shift();
-    console.log(`nextNode found = ${nextNode.node.innerHtml}`)
+    console.log(`nextNode found = ${nextNode.node}`)
     
     // Now let’s start iterating through the nodes. We are 
     // starting at i = 0, iterating until we hit 
