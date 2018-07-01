7ec3cce9df26a415e7b703e5e5b7198c2ea9dd55
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-01 10:31:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested').querySelector('.target')
 }
+
+function increaseRankBy(n) {
+  var ranks = document.querySelector('.ranked-List');
+  for(var i = 0; i < ranks.length; i++) {
+    ranks[i].textContent = i + parseInt(n);
+  }
+}
+
