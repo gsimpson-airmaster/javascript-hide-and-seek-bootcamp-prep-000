d2aa9137581483834dfcfd31396dc56231233bab
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-01 10:46:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,20 +6,14 @@ function nestedTarget() {
   return document.querySelector('#nested').querySelector('.target')
 }
 
-// function increaseRankBy(n) {
-//   var ranksParent = document.querySelectorAll('.ranked-list');
-//   for(var i = 0; i < ranksParent.length; i++) {
-// //    var ranks = ranksParent.querySelectorAll('li');
-//     for(var idx = 0; idx < ranks.length;idx++) {
-//       ranks[i].textContent = (parseInt(ranks[i].textContent) + parseInt(n));
-//     }
-//   }
-// }
-
 function increaseRankBy(n) {
   var ranks = document.querySelectorAll('.ranked-list li');
   for(var i = 0; i < ranks.length; i++) {
       ranks[i].textContent = (parseInt(ranks[i].textContent) + parseInt(n));
   }
+}
+
+function deepestChild() {
+  
 }
 
